ae1a6beaa8a9841469713eddb9f3525e6dab6631
Nelly Cheruiyot <nellycheruiyot@gmail.com>
2017-12-09 23:58:00 +0300

    Add tests to test name property, list all Squad objects, empty list during dev process, assign unique ID, locate a specific squad based on its ID and implemented the logics behind the tests

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,7 +1,35 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Squad {
   private String mName;
+  private static List<Squad> instances = new ArrayList<Squad>();
+  private int mId;
 
   public Squad(String name) {
     mName = name;
+    instances.add(this);
+    mId = instances.size();
   }
+
+  public String getName() {
+    return mName;
+  }
+
+  public static List<Squad> all() {
+    return instances;
+  }
+
+  public static void clear() {
+    instances.clear();
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static Squad find(int id) {
+    return instances.get(id-1);
+  }
+
 }
--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -8,4 +8,39 @@ public class SquadTest {
     Squad testSquad = new Squad("Feminists");
     assertEquals(true, testSquad instanceof Squad);
   }
+
+  @Test
+  public void getName_squadInstantiatesWithName_Feminists() {
+    Squad testSquad = new Squad("Feminists");
+    assertEquals("Feminists", testSquad.getName());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfSquad_true() {
+    Squad firstSquad = new Squad("Feminists");
+    Squad secondSquad = new Squad("Superheros");
+    assertEquals(true, Squad.all().contains(firstSquad));
+    assertEquals(true, Squad.all().contains(secondSquad));
+  }
+
+  @Test
+  public void clear_emptiesAllSquadsFromList_0() {
+    Squad testSquad = new Squad("Feminists");
+    Squad.clear();
+    assertEquals(Squad.all().size(), 0);
+  }
+
+  @Test
+  public void getId_squadsInstantiatesWithAnId_1() {
+    Squad testSquad = new Squad("Feminists");
+    assertEquals(1, testSquad.getId());
+  }
+
+  @Test
+  public void find_returnsSquadWithSameId_secondSquad() {
+    Squad.clear();
+    Squad firstSquad = new Squad("Feminists");
+    Squad secondSquad = new Squad("Superheros");
+    assertEquals(Squad.find(secondSquad.getId()), secondSquad);
+  }
 }
